8ec73eb59880dfd6b1f91b9fba51235c76b6cc7c
Bhavishya Negi <psspl.trainee32@outlook.com>
2020-03-05 18:36:08 +0530

    add product functionality

--- src/App.js
+++ src/App.js
@@ -7,22 +7,111 @@ import Upcoming from './components/upcoming'
 import MainSectionRight from './components/MainSectionRight'
 import './styles/card-left.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="main-right-section">
-        <div>
-          <Upcoming />
-          <Card />
-          <Yesterday />
-        </div>
-        <div>
-          <MainSectionRight />
+let id = 0
+
+let data = [
+  {
+    image: "https://ph-files.imgix.net/579db417-b025-443e-baa7-42b73ab72499?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop",
+    title: "Basecamp Personal",
+    text: "Manage all your small projects for free",
+    comments: "12",
+    upVote: "25431",
+    device: "iPhone",
+    id: id++
+  },
+
+  {
+    image: "https://ph-files.imgix.net/00cc62e7-fbbf-40b4-be56-9a9d58579a47?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop",
+    title: "Basecamp Personal",
+    text: "Manage all your small projects for free",
+    comments: "13",
+    upVote: "89431",
+    device: "Android",
+    id: id++
+  },
+  {
+    image: "https://ph-files.imgix.net/3b8fff05-5ac3-4e2b-b902-4606c677d40a?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop",
+    title: "Basecamp Personal",
+    text: "Manage all your small projects for free",
+    comments: "10",
+    upVote: "6531",
+    device: "Analytics",
+    id: id++
+  }
+]
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: data,
+      image: null,
+      text: null,
+      title: null
+    }
+  }
+
+  updateCount = id => {
+    let updatedData = this.state.data.map(p => {
+      if (p.id === id) {
+        p.upVote = Number(p.upVote) + 1;
+      }
+      return p;
+    });
+    this.setState({ data: updatedData });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.title && this.state.text) {
+      this.setState({
+        data: this.state.data.concat({
+          image: this.state.image,
+          text: this.state.text,
+          title: this.state.title,
+          upVote: "0",
+          comments: "10",
+          device: "Analytics",
+          id: id++
+        })
+      })
+    } else {
+      alert("Form cannot be empty!!!")
+    }
+    this.clear()
+  }
+
+  clear = () => {
+    this.setState({
+      title: "",
+      text: "",
+      image: ""
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="main-right-section">
+          <div>
+            <Upcoming data={this.state.data} updateCount={this.updateCount} />
+            <Card />
+            <Yesterday />
+          </div>
+          <div>
+            <MainSectionRight submit={this.onSubmit} handleChange={this.handleChange} state={this.state} />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
--- src/components/MainSectionRight.js
+++ src/components/MainSectionRight.js
@@ -1,45 +1,59 @@
 import React from 'react'
 import '../styles/style.scss'
 
-export default function MainSectionRight() {
-  return (
-    <div className="main-right">
-      <p className="makers_top">Makers Working Today</p>
-      <div className="makers_container">
-        <div className="makers_img_container">
-          {["1", "2", "3", "4", "5", "6"].map((person, index) => (
-            <img
-              className="makers_img"
-              src={`images/i${index + 1}.jpeg `}
-              alt=""
-            />
-          ))}
-          {["1", "2", "3", "4", "5", "6"].map((person, index) => (
-            <img
-              className="makers_img"
-              src={`images/i${index + 6}.jpeg `}
-              alt=""
-            />
-          ))}
-        </div>
-        <p className="join">JOIN THEM ON MAKERS</p>
-      </div>
-      <p className="product_hunt">Product Hunt Radio</p>
-      <div className="product_hunt_container">
-        <div className="product_hunt_left">
-          <p className="product_hunt_text">
-            The future of seed stage VC with Tige Savage
-          </p>
-        </div>
-        <div className="product_hunt_right">
-          <img
-            className="product_hunt_img"
-            src="images/product_hunt.png"
-            alt=""
-          />
-        </div>
-      </div>
 
-    </div>
-  )
+export default class MainSectionRight extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <div className="main-right">
+          <p className="makers_top">Makers Working Today</p>
+          <div className="makers_container">
+            <div className="makers_img_container">
+              {["1", "2", "3", "4", "5", "6"].map((person, index) => (
+                <img
+                  className="makers_img"
+                  src={`images/i${index + 1}.jpeg `}
+                  alt=""
+                />
+              ))}
+              {["1", "2", "3", "4", "5", "6"].map((person, index) => (
+                <img
+                  className="makers_img"
+                  src={`images/i${index + 6}.jpeg `}
+                  alt=""
+                />
+              ))}
+            </div>
+            <p className="join">JOIN THEM ON MAKERS</p>
+          </div>
+          <p className="product_hunt">Product Hunt Radio</p>
+          <div className="product_hunt_container">
+            <div className="product_hunt_left">
+              <p className="product_hunt_text">
+                The future of seed stage VC with Tige Savage
+          </p>
+            </div>
+            <div className="product_hunt_right">
+              <img
+                className="product_hunt_img"
+                src="images/product_hunt.png"
+                alt=""
+              />
+            </div>
+
+          </div>
+          <p className="product_hunt" style={{ "position": "relative", "bottom": "5rem" }}>Add Products</p>
+          <form onSubmit={(e) => this.props.submit(e)} className="right_form">
+            <input type="text" value={this.props.state.title} name="title" onChange={(e) => this.props.handleChange(e)} placeholder="Enter Product Name" />
+            <input type="text" value={this.props.state.text} name="text" onChange={(e) => this.props.handleChange(e)} placeholder="Enter Description" />
+            <input type="text" value={this.props.state.image} name="image" onChange={(e) => this.props.handleChange(e)} placeholder="Image URL" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
+
